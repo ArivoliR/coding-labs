8ea959d6e58e8aac2a03ca89c45e484f30b43218
ArivoliR <arivoli2005@gmail.com>
2026-05-30 15:40:35 +0530

    Implement tokenize
    Signed-off-by: ArivoliR <arivoli2005@gmail.com>

--- assignment3-ispell-templates-concepts/spellcheck.cpp
+++ assignment3-ispell-templates-concepts/spellcheck.cpp
@@ -1,7 +1,9 @@
 #include "spellcheck.h"
 
 #include <algorithm>
+#include <cinttypes>
 #include <iostream>
+#include <iterator>
 #include <numeric>
 #include <ranges>
 #include <set>
@@ -10,12 +12,24 @@
 template <typename Iterator, typename UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
-Corpus tokenize(std::string& source) {
-  /* TODO: Implement this method */
-  return Corpus();
+Corpus tokenize(std::string &source) {
+  Corpus corpus;
+  auto spaces = find_all(source.begin(), source.end(), ::isspace);
+  auto make_token = [&](auto begin, auto end) {
+    return Token(source, begin, end);
+  };
+
+  std::transform(spaces.begin(), std::prev(spaces.end()),
+                 std::next(spaces.begin()), std::inserter(corpus, corpus.end()),
+                 make_token);
+  auto empty_token = [](const Token &token) { return token.content.empty(); };
+  std::erase_if(corpus, empty_token);
+
+  return corpus;
 }
 
-std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
+std::set<Misspelling> spellcheck(const Corpus &source,
+                                 const Dictionary &dictionary) {
   /* TODO: Implement this method */
   return std::set<Misspelling>();
 };
